4690cff216ccc42dc4f9080a64a32568366e476c
Janjancel <128217434+Janjancel@users.noreply.github.com>
2024-09-27 16:52:21 +0800

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -13,7 +13,7 @@ export const TodoForm = ({addTodo}) => {
   return (
     <form className='TodoForm' onSubmit={handleSubmit}>
         <input type="text" className='todo-input' value={value}
-        placeholder='what is the task today?' 
+        placeholder='Enter your task here!' 
         onChange={(e) => setValue(e.target.value)} required />
         <button type="submit" className='todo-btn'>Add Task</button>
     </form>
